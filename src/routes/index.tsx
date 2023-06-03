1b4ccf50eb59a397b5ee656c186a1e1f390664bb
Matthew Arthur <matthewarthur474@gmail.com>
2023-06-03 12:54:42 -0700

    Add more to landing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,27 @@
 const Index = () => {
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-10 px-40">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 px-40">
       <h1 className="text-center text-7xl font-black">
         Save the{" "}
-        <i className="bg-gradient-to-tr from-green-400 to-blue-600 bg-clip-text not-italic text-transparent">
+        <i className="bg-gradient-to-tr from-green-400 to-green-600 bg-clip-text not-italic text-transparent">
           environment
         </i>{" "}
         with a scan.
       </h1>
-      <div className="flex flex-col gap-2">
-        <button className="h-12 w-96 rounded-lg bg-green-400 shadow-sm">
-          Scan your object
+      <div className="flex flex-col gap-2 text-white">
+        <button className="h-12 w-96 rounded-lg bg-black shadow-sm">
+          Get a picture
         </button>
-        <input
-          className="h-12 w-96 rounded-lg border-2 border-solid border-green-400 p-2 shadow-inner"
-          type="text"
-          placeholder="Or use text"
-        />
+        <div className="flex h-12 w-96 items-center">
+          <input
+            className="h-full flex-grow rounded-lg rounded-r-none border-2 border-solid border-black p-2 text-black shadow-inner focus:outline-0"
+            type="text"
+            placeholder="Or use text"
+          />
+          <button className="flex h-12 w-12 items-center justify-center rounded-r-lg bg-black">
+            <i className="bi-search text-lg text-white" />
+          </button>
+        </div>
       </div>
     </div>
   );
